14230e2ee8b9d24932b877bc5a83c292cf69b3dd
TauruS111 <yuriybereza85@gmail.com>
2024-01-13 16:30:27 +0100

    finish

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { persistor } from './slice/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
-    </React.StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
